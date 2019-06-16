6d509b5878f3ef08916dd9c5e15c542b92966739
lbovolini <lbovolini94@gmail.com>
2019-06-16 18:45:29 -0300

    Changed library name

--- examples/native/native/src/main/java/com/github/lbovolini/example/nativeLib/Native.java
+++ examples/native/native/src/main/java/com/github/lbovolini/example/nativeLib/Native.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class Native implements RemoteNative, Serializable {
 
     static {
-        System.loadLibrary("hellonative");
+        System.loadLibrary("hello");
     }
 
     public native void say(String str);
