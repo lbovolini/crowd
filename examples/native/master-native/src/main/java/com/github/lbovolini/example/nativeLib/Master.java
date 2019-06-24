ac114ea49e3138d8cef502ca384db883f7bcf2bf
lbovolini <lbovolini94@gmail.com>
2019-06-23 22:21:15 -0300

    Fixed native library example

--- examples/native/master-native/src/main/java/com/github/lbovolini/example/nativeLib/Master.java
+++ examples/native/master-native/src/main/java/com/github/lbovolini/example/nativeLib/Master.java
@@ -8,7 +8,7 @@ public class Master {
 
     public static void main(String[] args) throws IOException {
 
-        NodeGroup<RemoteNative> nodeGroup = new NodeGroup<>(Native.class.getName());
+        NodeGroup<RemoteNative> nodeGroup = new NodeGroup<>(Call.class.getName());
         nodeGroup.forOne(remoteNative -> remoteNative.say("World"));
     }
 
