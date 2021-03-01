9de7e204ba8ac43c84e530ef925824498c8811fc
Lucas Bovolini <lbovolini94@gmail.com>
2021-03-01 18:59:14 -0300

    Fixed network interface search strategy

--- crowd/src/main/java/com/github/lbovolini/crowd/utils/HostUtils.java
+++ crowd/src/main/java/com/github/lbovolini/crowd/utils/HostUtils.java
@@ -5,14 +5,25 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Enumeration;
+import java.util.List;
 
 public interface HostUtils {
+
+    /**
+     * Busca e retorna o primeiro endereço IP v4 da primeira interface de rede.
+     * @return Endereço IP v4.
+     * @throws SocketException
+     */
     static String getHostAddress() throws SocketException {
 
         Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
 
-        for (NetworkInterface netint : Collections.list(nets)) {
+        List<NetworkInterface> netInterfaces = Collections.list(nets);
+        netInterfaces.sort(Comparator.comparingInt(NetworkInterface::getIndex));
+
+        for (NetworkInterface netint : netInterfaces) {
             Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
 
             for (InetAddress inetAddress : Collections.list(inetAddresses)) {
@@ -31,15 +42,24 @@ public interface HostUtils {
 
         try {
             address = getHostAddress();
-        } catch (SocketException e) { }
+        } catch (SocketException ignored) { }
+
         return address;
     }
 
+    /**
+     * Busca e retorna o nome da primeira interface de rede.
+     * @return O nome da interface de rede.
+     * @throws SocketException
+     */
     static String getNetworkInterface() throws SocketException {
 
         Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
 
-        for (NetworkInterface netint : Collections.list(nets)) {
+        List<NetworkInterface> netInterfaces = Collections.list(nets);
+        netInterfaces.sort(Comparator.comparingInt(NetworkInterface::getIndex));
+
+        for (NetworkInterface netint : netInterfaces) {
             Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
 
             for (InetAddress inetAddress : Collections.list(inetAddresses)) {
@@ -49,6 +69,7 @@ public interface HostUtils {
                 }
             }
         }
+
         return "";
     }
 
@@ -57,7 +78,8 @@ public interface HostUtils {
 
         try {
             netInt = getNetworkInterface();
-        } catch (SocketException e) {  }
+        } catch (SocketException ignored) {  }
+
         return netInt;
     }
 }
