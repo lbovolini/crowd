3ffeb2097168d15545c70cc2ef58890e20866a02
Lucas Bovolini <lbovolini94@gmail.com>
2021-03-01 21:00:34 -0300

    Added HostUtils test cases

---
+++ crowd/src/test/java/com/github/lbovolini/crowd/utils/HostUtilsTest.java
@@ -0,0 +1,22 @@
+package com.github.lbovolini.crowd.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HostUtilsTest {
+
+    @Test
+    void shouldReturnNetworkInterfaceName() {
+        String name = HostUtils.getNetworkInterfaceName();
+
+        assertTrue(name.length() > 0);
+    }
+
+    @Test
+    void shouldReturnHostAddressName() {
+        String name = HostUtils.getHostAddressName();
+
+        assertTrue(name.length() > 0);
+    }
+}
