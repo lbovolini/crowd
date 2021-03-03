f165847294b1bb34709bc4ee4a40b35beec5ffd6
Lucas Bovolini <lbovolini94@gmail.com>
2021-03-03 16:36:37 -0300

    Added Connection test cases

---
+++ crowd/src/test/java/com/github/lbovolini/crowd/group/ConnectionTest.java
@@ -0,0 +1,59 @@
+package com.github.lbovolini.crowd.group;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.net.InetSocketAddress;
+
+import static com.github.lbovolini.crowd.configuration.Config.HEARTBEAT;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ConnectionTest {
+
+    @Mock
+    private Multicast multicast;
+
+    @InjectMocks
+    private Connection connection;
+
+    @Test
+    void shouldSendMessageToServer() {
+        // Input
+        String messageType = HEARTBEAT;
+
+        // Should test ONLY this method
+        connection.send(messageType);
+
+        // Assertions
+        verify(multicast, only()).send(messageType);
+    }
+
+    @Test
+    void shouldSendMessageToAllHosts() {
+        // Input
+        String messageType = HEARTBEAT;
+
+        // Should test ONLY this method
+        connection.sendAll(messageType);
+
+        // Assertions
+        verify(multicast, only()).sendAll(messageType);
+    }
+
+    @Test
+    void shouldSendMessageToSpecificHost() {
+        // Input
+        String messageType = HEARTBEAT;
+        InetSocketAddress address = InetSocketAddress.createUnresolved("localhost", 0);
+
+        // Should test ONLY this method
+        connection.sendToHost(messageType, address);
+
+        // Assertions
+        verify(multicast, only()).sendToHost(messageType, address);
+    }
+}
