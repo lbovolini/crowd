ba8aad1a2afc47a9fa02910076b9f3ed8e0667c3
Lucas Bovolini <lbovolini94@gmail.com>
2021-03-22 21:11:26 -0300

    Added test case

--- crowd/src/test/java/com/github/lbovolini/crowd/group/MulticastConnectionTest.java
+++ crowd/src/test/java/com/github/lbovolini/crowd/group/MulticastConnectionTest.java
@@ -4,7 +4,6 @@ import com.github.lbovolini.crowd.group.connection.MulticastConnection;
 import com.github.lbovolini.crowd.group.connection.MulticastReaderChannel;
 import com.github.lbovolini.crowd.group.connection.MulticastWriterChannel;
 import com.github.lbovolini.crowd.group.message.MulticastMessageType;
-import com.github.lbovolini.crowd.group.worker.MulticastWorker;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -25,7 +24,7 @@ class MulticastConnectionTest {
     private MulticastWriterChannel writerChannel;
 
     @InjectMocks
-    private MulticastConnection multicastConnection;
+    private MulticastConnection connection;
 
     @Test
     void shouldSendMessageToServer() {
@@ -33,7 +32,7 @@ class MulticastConnectionTest {
         MulticastMessageType messageType = HEARTBEAT;
 
         // Should test ONLY this method
-        multicastConnection.send(messageType);
+        connection.send(messageType);
 
         // Assertions
         verify(writerChannel, only()).write(messageType.getType());
@@ -45,7 +44,7 @@ class MulticastConnectionTest {
         MulticastMessageType messageType = HEARTBEAT;
 
         // Should test ONLY this method
-        multicastConnection.multicastSend(messageType);
+        connection.multicastSend(messageType);
 
         // Assertions
         verify(writerChannel, only()).writeGroup(messageType.getType());
@@ -58,9 +57,18 @@ class MulticastConnectionTest {
         InetSocketAddress address = InetSocketAddress.createUnresolved("localhost", 0);
 
         // Should test ONLY this method
-        multicastConnection.send(messageType, address);
+        connection.send(messageType, address);
 
         // Assertions
         verify(writerChannel, only()).write(messageType.getType(), address);
+    }
+
+    @Test
+    void shouldReceiveMessage() {
+        // Should test ONLY this method
+        connection.receive();
+
+        // Assertions
+        verify(readerChannel, only()).read();
     }
 }
