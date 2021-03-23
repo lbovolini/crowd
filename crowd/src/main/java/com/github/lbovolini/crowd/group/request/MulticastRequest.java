5ec3947afe764a43f7babc7a641798bba6db495a
Lucas Bovolini <lbovolini94@gmail.com>
2021-03-23 14:08:23 -0300

    Added equals and hashcode

--- crowd/src/main/java/com/github/lbovolini/crowd/group/request/MulticastRequest.java
+++ crowd/src/main/java/com/github/lbovolini/crowd/group/request/MulticastRequest.java
@@ -3,6 +3,8 @@ package com.github.lbovolini.crowd.group.request;
 import com.github.lbovolini.crowd.group.connection.MulticastConnection;
 import com.github.lbovolini.crowd.group.message.MulticastMessage;
 
+import java.util.Objects;
+
 public class MulticastRequest {
 
     private final MulticastConnection connection;
@@ -19,5 +21,18 @@ public class MulticastRequest {
 
     public MulticastMessage getMessage() {
         return message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MulticastRequest that = (MulticastRequest) o;
+        return Objects.equals(connection, that.connection) && Objects.equals(message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(connection, message);
     }
 }
