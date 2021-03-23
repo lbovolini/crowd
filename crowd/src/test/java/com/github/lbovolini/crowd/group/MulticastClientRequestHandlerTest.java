1fea5a5f24847a051f7cd300e38c9738e028d605
Lucas Bovolini <lbovolini94@gmail.com>
2021-03-22 21:08:55 -0300

    Fixed test cases

--- crowd/src/test/java/com/github/lbovolini/crowd/group/MulticastClientRequestHandlerTest.java
+++ crowd/src/test/java/com/github/lbovolini/crowd/group/MulticastClientRequestHandlerTest.java
@@ -1,16 +1,20 @@
 package com.github.lbovolini.crowd.group;
 
+import com.github.lbovolini.crowd.group.message.MulticastMessage;
+import com.github.lbovolini.crowd.group.message.MulticastMessageType;
+import com.github.lbovolini.crowd.group.request.MulticastClientRequestHandler;
+import com.github.lbovolini.crowd.group.request.MulticastRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static com.github.lbovolini.crowd.configuration.Config.*;
+import static com.github.lbovolini.crowd.group.message.MulticastMessageType.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class MulticastWorkerClientRequestHandlerTest {
+class MulticastClientRequestHandlerTest {
 
     @Mock
     private CodebaseService codebaseService;
@@ -21,7 +25,7 @@ class MulticastWorkerClientRequestHandlerTest {
     @Test
     void shouldHandleConnectRequest() {
         // Input
-        Request request = new Request(null, MulticastMessage.ofType(CONNECT, null));
+        MulticastRequest request = new MulticastRequest(null, MulticastMessage.ofType(CONNECT, null));
 
         // Should test ONLY this method
         requestHandler.handle(request);
@@ -32,7 +36,7 @@ class MulticastWorkerClientRequestHandlerTest {
     @Test
     void shouldHandleUpdateRequest() {
         // Input
-        Request request = new Request(null, MulticastMessage.ofType(UPDATE, null));
+        MulticastRequest request = new MulticastRequest(null, MulticastMessage.ofType(UPDATE, null));
 
         // Should test ONLY this method
         requestHandler.handle(request);
@@ -43,7 +47,7 @@ class MulticastWorkerClientRequestHandlerTest {
     @Test
     void shouldHandleReloadRequest() {
         // Input
-        Request request = new Request(null, MulticastMessage.ofType(RELOAD, null));
+        MulticastRequest request = new MulticastRequest(null, MulticastMessage.ofType(RELOAD, null));
 
         // Should test ONLY this method
         requestHandler.handle(request);
