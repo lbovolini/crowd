0c7e2cca3e68f4b3acc73b7891588ab0532ab6bf
Lucas Bovolini <lbovolini94@gmail.com>
2021-05-12 18:16:15 -0300

    Fixed android compatibility by preventing string concatenation optimization in bytecode since java 9

--- examples/hello/com.github.lbovolini.crowd.example.hello/src/main/java/com/github/lbovolini/crowd/example/hello/Hello.java
+++ examples/hello/com.github.lbovolini.crowd.example.hello/src/main/java/com/github/lbovolini/crowd/example/hello/Hello.java
@@ -4,9 +4,9 @@ public class Hello implements IHello<String> {
     int x;
 
     public String say(Integer i) throws InterruptedException {
-        System.out.println(i + " Hello world");
+        System.out.println(String.valueOf(i).concat(" Hello world"));
         Thread.sleep(1000);
-        String response = i + " done " + x++;
+        String response = String.valueOf(i).concat(" done ").concat(String.valueOf(x++));
         return response;
     }
 }
