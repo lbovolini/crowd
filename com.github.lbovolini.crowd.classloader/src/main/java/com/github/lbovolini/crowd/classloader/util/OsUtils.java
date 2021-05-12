85a6d19fc654ba7489566ca20845781a4a50980e
Lucas Bovolini <lbovolini94@gmail.com>
2021-05-12 17:56:36 -0300

    Fixed arch and OS names

--- com.github.lbovolini.crowd.classloader/src/main/java/com/github/lbovolini/crowd/classloader/util/OsUtils.java
+++ com.github.lbovolini.crowd.classloader/src/main/java/com/github/lbovolini/crowd/classloader/util/OsUtils.java
@@ -2,14 +2,14 @@ package com.github.lbovolini.crowd.classloader.util;
 
 public class OsUtils {
 
-    public static final String OS_ARCH = System.getProperty("os.arch");
-    public static final String VENDOR = System.getProperty("java.vendor");
-    public static final String OS_NAME = System.getProperty("os.name");
+    public static final String OS_ARCH = System.getProperty("os.arch").toLowerCase();
+    public static final String VENDOR = System.getProperty("java.vendor").toLowerCase();
+    public static final String OS_NAME = System.getProperty("os.name").toLowerCase();
 
     private static final String ARCH_x86    = "x86";
     private static final String ARCH_x86_64 = "x86_64";
-    private static final String ARCH_ARM_32 = "arm32";
-    private static final String ARCH_ARM_64 = "arm64";
+    private static final String ARCH_ARM_32 = "armeabi-v7a";
+    private static final String ARCH_ARM_64 = "arm64-v8a";
 
     private static final String OS_WINDOWS = "windows";
     private static final String OS_LINUX   = "linux";
@@ -17,16 +17,16 @@ public class OsUtils {
 
     public static String getArch() {
 
-        if (OS_ARCH.contains("amd64") || OS_ARCH.contains("x86_64")) {
+        if (OS_ARCH.equals("amd64") || OS_ARCH.equals("x86_64") || OS_ARCH.equals("x86-64")) {
             return ARCH_x86_64;
         }
-        if (OS_ARCH.contains("x86")) {
+        if (OS_ARCH.equals("x86") || OS_ARCH.equals("x86_32") || OS_ARCH.equals("i686")) {
             return ARCH_x86;
         }
-        if (OS_ARCH.contains("armeabi") || OS_ARCH.contains("armv7")) {
+        if (OS_ARCH.equals("armeabi")) {
             return ARCH_ARM_32;
         }
-        if (OS_ARCH.contains("arm64")) {
+        if (OS_ARCH.equals("aarch64")) {
             return ARCH_ARM_64;
         }
 
@@ -35,13 +35,13 @@ public class OsUtils {
 
     public static String getOs() {
 
-        if (VENDOR.contains("Android")) {
+        if (VENDOR.contains("android")) {
             return OS_ANDROID;
         }
-        if (OS_NAME.contains("Linux")) {
+        if (OS_NAME.contains("linux")) {
             return OS_LINUX;
         }
-        if (OS_NAME.contains("Windows")) {
+        if (OS_NAME.contains("windows")) {
             return OS_WINDOWS;
         }
 
