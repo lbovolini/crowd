3d1ad518b691ade136ee9aebe47552b089b1c200
Lucas Bovolini <lbovolini94@gmail.com>
2021-05-31 11:54:38 -0300

    Added equals and hashcode

--- com.github.lbovolini.crowd.core/src/main/java/com/github/lbovolini/crowd/core/message/messages/JoinGroup.java
+++ com.github.lbovolini.crowd.core/src/main/java/com/github/lbovolini/crowd/core/message/messages/JoinGroup.java
@@ -1,8 +1,10 @@
 package com.github.lbovolini.crowd.core.message.messages;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class JoinGroup implements Serializable {
+
     private final int cores;
 
     public JoinGroup(int cores) {
@@ -11,5 +13,25 @@ public class JoinGroup implements Serializable {
 
     public int getCores() {
         return cores;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        JoinGroup joinGroup = (JoinGroup) o;
+
+        return cores == joinGroup.cores;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cores);
     }
 }
