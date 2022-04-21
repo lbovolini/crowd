a5927152961bb6472bb71718df34d36daf7971bb
lbovolini <lbovolini94@gmail.com>
2022-04-21 14:14:02 -0300

    Removed Android support

--- com.github.lbovolini.crowd.android.classloader/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.github.lbovolini.crowd.android {
-    requires com.github.lbovolini.crowd.classloader;
-    requires dalvik.dx;
-    requires transitive android;
-
-    exports com.github.lbovolini.crowd.android.classloader;
-}
